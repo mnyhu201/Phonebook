162b09a3af4a3c54e9ff6a5aa39184fb63ddde98
Minyu Huang <mnyhu201@gmail.com>
2024-10-06 20:46:30 -0700

    Remove node_modules from tracking

--- client/src/Filter.js
+++
@@ -1,8 +0,0 @@
-const Filter = (props) => {
-    const {onChange, value} = props;
-    return (<input value={value} onChange={onChange}></input>)
-}
-
-
-
-export default Filter;
--- client/src/Message.js
+++
@@ -1,9 +0,0 @@
-const Message = (props) => {
-    const {value} = props;
-    if(value != null){
-        return <p className="errorMessage">{value}</p>
-    }
-    return <></>
-}
-
-export default Message;
